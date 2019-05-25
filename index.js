98f281ae2da36b3ec542cb9b19e28bbe4bc7451e
Daniel <danieldotinc@gmail.com>
2019-05-25 21:31:40 +0430

    add test

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ require("./startup/db")();
 require("./startup/routes")(app);
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () =>
-  console.log(`Listening on port ${port} ...`)
-);
+const server = app.listen(port, () => console.log(`Listening on ${port} ...`));
 
 module.exports = server;
